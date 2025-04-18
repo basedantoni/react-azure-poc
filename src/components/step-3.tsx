668bb05ad5c0d7cda7cad0a56d8ccd21e51471ab
basedantoni <anthony.mercado300@gmail.com>
2025-04-18 12:09:04 -0500

    fix: unused variable lint

--- src/components/step-3.tsx
+++ src/components/step-3.tsx
@@ -1,6 +1,24 @@
-import { UseFormReturn } from 'react-hook-form';
+import { Input } from './ui/input';
+import { FormField, FormItem, FormLabel, FormControl } from './ui/form';
+
 import { FormValues } from '@/schema';
+import { UseFormReturn } from 'react-hook-form';
 
 export function Step3({ form }: { form: UseFormReturn<FormValues> }) {
-  return <div>Step3</div>;
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name='fullTicketQuantity'
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Full Ticket Quantity</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </>
+  );
 }
--- src/components/step-4.tsx
+++ src/components/step-4.tsx
@@ -1,6 +1,24 @@
+import { FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import { Input } from './ui/input';
+
 import { UseFormReturn } from 'react-hook-form';
 import { FormValues } from '@/schema';
 
 export function Step4({ form }: { form: UseFormReturn<FormValues> }) {
-  return <div>Step4</div>;
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name='fullTicketQuantity'
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Full Ticket Quantity</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </>
+  );
 }
