a74637d1ea4cd2cb6109e14648cf95f992fef886
basedantoni <anthony.mercado300@gmail.com>
2025-04-18 12:32:42 -0500

    feat: build skeletons of step 3 and 4

--- src/components/step-3.tsx
+++ src/components/step-3.tsx
@@ -1,24 +1,65 @@
+import { FormField } from './ui/form';
 import { Input } from './ui/input';
-import { FormField, FormItem, FormLabel, FormControl } from './ui/form';
 
-import { FormValues } from '@/schema';
 import { UseFormReturn } from 'react-hook-form';
+import { FormValues } from '@/schema';
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableFooter,
+} from './ui/table';
 
 export function Step3({ form }: { form: UseFormReturn<FormValues> }) {
   return (
     <>
-      <FormField
-        control={form.control}
-        name='fullTicketQuantity'
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Full Ticket Quantity</FormLabel>
-            <FormControl>
-              <Input {...field} />
-            </FormControl>
-          </FormItem>
-        )}
-      />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Type of Ticket</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Amount Due</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Full Ticket (meal ticket included)</TableCell>
+            <TableCell>
+              <FormField
+                control={form.control}
+                name='fullTicketQuantity'
+                render={({ field }) => <Input type='number' {...field} />}
+              />
+            </TableCell>
+            <TableCell>$0</TableCell>
+            <TableCell>$0</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Meal Ticket</TableCell>
+            <TableCell>
+              <FormField
+                control={form.control}
+                name='mealTicketQuantity'
+                render={({ field }) => <Input type='number' {...field} />}
+              />
+            </TableCell>
+            <TableCell>$0</TableCell>
+            <TableCell>$0</TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>Total</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>$0</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
     </>
   );
 }
--- src/components/step-4.tsx
+++ src/components/step-4.tsx
@@ -1,21 +1,71 @@
-import { FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from './ui/form';
 import { Input } from './ui/input';
 
 import { UseFormReturn } from 'react-hook-form';
 import { FormValues } from '@/schema';
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableFooter,
+} from './ui/table';
 
 export function Step4({ form }: { form: UseFormReturn<FormValues> }) {
   return (
     <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Type of Ticket</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Amount Due</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Full Ticket (meal ticket included)</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Meal Ticket</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>Total</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>$0</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
       <FormField
         control={form.control}
-        name='fullTicketQuantity'
+        name='email'
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Full Ticket Quantity</FormLabel>
+            <FormLabel>Email</FormLabel>
             <FormControl>
-              <Input {...field} />
+              <Input placeholder='Enter your email' type='email' {...field} />
             </FormControl>
+            <FormDescription>
+              Enter your email to recieve a copy of this order
+            </FormDescription>
           </FormItem>
         )}
       />
