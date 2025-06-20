b2953019baf4f7a09cdbdf1e74624760e2e64ee4
basedantoni <anthony.mercado300@gmail.com>
2025-06-20 16:34:34 -0500

    remove chunking from vite config

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,14 +1,5 @@
+import { SubmissionTableWrapper } from '@/components/submissions/submission-table-wrapper';
 import { createFileRoute } from '@tanstack/react-router';
-import { lazy, Suspense } from 'react';
-
-// Lazy load the submission table component
-const SubmissionTableWrapper = lazy(() =>
-  import('@/components/submissions/submission-table-wrapper').then(
-    (module) => ({
-      default: module.SubmissionTableWrapper,
-    })
-  )
-);
 
 export const Route = createFileRoute('/admin')({
   component: RouteComponent,
@@ -18,9 +9,7 @@ function RouteComponent() {
   return (
     <main className='flex flex-col py-8 container mx-auto justify-center space-y-8'>
       <h1 className='text-2xl font-bold'>Admin</h1>
-      <Suspense fallback={<div className='p-4'>Loading admin data...</div>}>
-        <SubmissionTableWrapper />
-      </Suspense>
+      <SubmissionTableWrapper />
     </main>
   );
 }
